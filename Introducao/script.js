30e79afb77e89a1e216c2cd2d1767140bc0bb5ed
Jacqueline dos Santos <jacquelinevps@hotmail.com>
2020-06-24 21:08:13 -0300

    #

---
+++ Introducao/script.js
@@ -0,0 +1,15 @@
+console.log('Olá Mundo!')
+let nome = 'Diego'
+let idade = 22
+let peso =  48
+let humano = true
+let alunos = ['Diego', 'Gabriel ', 'Gustavo']
+
+var aluno = {
+    nome: alunos[2],  //pode pegar um variavel de outro bloco
+    idade: 23,
+    peso:48,
+    humano: true
+}
+console.log(alunos)
+console.log(aluno)
